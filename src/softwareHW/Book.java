e2cb63ed7aa57f40b349a5a8eb27b9e23ce5a178
Ayman taha <Ayman_taha_1997@hotmail.com>
2019-02-19 05:24:10 -0800

    SoftwareHW

---
+++ src/softwareHW/Book.java
@@ -0,0 +1,19 @@
+package softwareHW;
+
+public class Book {
+	public String name;
+	public double price;
+	
+	Book(String n, double d) {
+		this.name = n;
+		this.price = d;
+	}
+	
+	public double getPrice() {
+		return this.price;
+	}
+	
+	public String getName() {
+		return this.name;
+	}
+}
